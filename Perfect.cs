e076b74009c334b7e3c96e2192787d6acbee8d0b
nine1een <solomon.justinm@gmail.com>
2015-12-10 08:59:39 -0600

    Create Perfect.cs

---
+++ Perfect.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Lab10
+{
+    class Perfect
+    {
+        static void Main(string[] args)
+        {
+            int sum,
+                num = 1,
+                div;
+            const int LIMIT = 1000;
+
+            while(num <= LIMIT)//loops 1000 times
+            {
+                sum = 0;
+                div = 1;
+
+                while(div < num)//while the dividen is less than the tested number
+                {
+                    if((num % div) == 0)//if dividen can multiplied evenly into num
+                    {
+                        sum += div;
+                    }
+
+                    ++div;
+                }
+
+                if (sum == num)//if num is perfect
+                    WriteLine("{0} ", num);
+
+                ++num;//add one to num
+            }
+        }
+    }
+}
